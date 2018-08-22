482ba7a90fc8c9581785ca787083f18c70a91e26
Elizabeth Karst <lkarst@outlook.com>
2018-08-22 01:24:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,11 @@ class Song
   @@genres = []
   @@genre_count = 0
 
-  
+  def initialize(name,artist,genre)
+    @@count += 1
+    @@artist << artist unless @@artist.include?(artist)
+    @@genre << genre unless @@genre.include?(genre)
+    @@genre_count += 1
+  end
 
 end
